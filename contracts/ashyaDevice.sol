1d9bafd200bd801cf57ae7978a7d5ec8356492f2
sanaAB <sanaabohelal@gmail.com>
2018-04-15 21:28:44 +0200

    make AshyaDevice owns it's Urls array and got rid of addresses requirments

--- contracts/ashyaDevice.sol
+++ contracts/ashyaDevice.sol
@@ -2,20 +2,18 @@ pragma solidity ^0.4.19;
 
 contract AshyaDevice {
 
-    // The address must match the one of the newly created AshyaRegistry Contract
-    address constant AshyaRegistryAddress=0x9dd1e8169e76a9226b07ab9f85cc20a5e1ed44dd;
+  // The address must match the one of the newly created AshyaRegistry Contract
+address constant AshyaRegistryAddress=0x692a70d2e424a56d2c6c27aa97d1a86395877b3a;
+string[] urls;
 
-    function AshyaDevice(string name, string location, string url, address ownerAddress) public payable {
-        AshyaRegistry DeviceObj = AshyaRegistry(AshyaRegistryAddress);
-        return DeviceObj.addItem(name,location,url, address(this), ownerAddress);  }
+function AshyaDevice(string name, string location, string url) public payable {
+    AshyaRegistry DeviceObj = AshyaRegistry(AshyaRegistryAddress);
+    DeviceObj.addItem(name,location,url);  }
 
-    function addNewUrl(string url) public {
-        AshyaRegistry DeviceObj = AshyaRegistry(AshyaRegistryAddress);
-        DeviceObj.addUrl(address(this), url);
-    }
+function addUrl(string url) public {
+    urls.push(url);}
 
-    function removeUrl(string url) public {
-        AshyaRegistry DeviceObj = AshyaRegistry(AshyaRegistryAddress);
-        DeviceObj.removeUrl(address(this), url);
-    }
+function getUrlsCount()public constant returns (uint count){
+    return urls.length;}
+
 }
--- contracts/ashyaRegistry.sol
+++ contracts/ashyaRegistry.sol
@@ -1,78 +1,67 @@
 pragma solidity ^0.4.19;
 
 contract AshyaRegistry {
-    struct item {
-        
-        string iname;
-        string location;
-        string url;
-        address ownerAddress;
-        uint index;
-        
-        }   
-
-    mapping(address => item) itemList;
-    address[] itemIndex;
-    
-    event LogNewItem (address indexed itemAddress, uint index, string iname, string location, string url, address ownerAddress);
-    event LogDeleteItem(address indexed itemAddress, uint index);
-    
-    function AshyaRegistry() public {
-        itemIndex.length = 0;
+struct item {
+    string iname;
+    string location;
+    string url;
+    address ownerAddress;
+    uint index;
     }
 
-    function addItem(string name, string location, string url) public  {        
-        //var itemnew = item(name ,url, location, itemAddress);
-        itemList[itemAddress].iname = name;
-        itemList[itemAddress].location = location;
-        itemList[itemAddress].url = url
-        itemList[itemAddress].ownerAddress = msg.sender
-        itemList[itemAddress].index = itemIndex.push(itemAddress)-1;
-        
-        emit LogNewItem(
-            itemAddress,
-            itemList[itemAddress].index,
-            name,
-            location,
-            url,
-            ownerAddress);
-    }
+mapping(address => item)public itemList;
+address[] itemIndex;
 
-    function getItemCount()public constant returns (uint count) {     
-        return itemIndex.length;
-    }
-    
-    modifier onlyBy(address _itemAddress) {
-        require(msg.sender == itemList[_itemAddress].ownerAddress || msg.sender == address(this));
-        _;
-    }
-    
-//    function checkOwnership(address itemAddress) public{
-//        require(msg.sender == itemList[itemAddress].ownerAddress || msg.sender == address(this));
-//    } 
+event LogNewItem (address indexed itemAddress, uint index, string iname, string location, string url, address ownerAddress);
+event LogDeleteItem(address indexed itemAddress, uint index);
 
-    function removeItem(address _itemAddress) public onlyBy(_itemAddress) {
-        //checkOwnership(itemAddress);
-        uint rowToDelete = itemList[_itemAddress].index;
-        address keyToMove = itemIndex[itemIndex.length-1];
-        itemIndex[rowToDelete] = keyToMove;
-        itemList[keyToMove].index = rowToDelete;
-        itemIndex.length--;
-        emit LogDeleteItem(
-            _itemAddress,
-            rowToDelete);
-    }
-    
-    function getItem(address itemAddress)public constant returns (string name, uint url, string location, uint index, address ownerAddress) {   
-        return (itemList[itemAddress].iname, itemList[itemAddress].urls.length, itemList[itemAddress].location, itemList[itemAddress].index, itemList[itemAddress].ownerAddress);
-    }
-    
-    function getItemAtIndex(uint index) public constant returns(address itemAddress) {
-        return itemIndex[index];
-    }
-    
-    function addUrl(address itemAddress, string url) public {
-        itemList[itemAddress].urls.push(url);
-    }
+function AshyaRegistry() public {
+    itemIndex.length = 0;}
+
+function addItem(string name, string location, string url) public  {
+    //var itemnew = item(name ,url, location, itemAddress);
+    itemList[msg.sender].iname = name;
+    itemList[msg.sender].location = location;
+    itemList[msg.sender].url= url;
+    itemList[msg.sender].ownerAddress = msg.sender;
+    itemList[msg.sender].index = itemIndex.push(msg.sender)-1;
+
+ emit  LogNewItem(
+        msg.sender,
+        itemList[msg.sender].index,
+        name,
+        location,
+        url,
+        msg.sender);
 }
 
+function getItemCount()public constant returns (uint count) {
+    return itemIndex.length;}
+
+modifier onlyBy(address _itemAddress) {
+    require(msg.sender == itemList[_itemAddress].ownerAddress || msg.sender == address(this));
+    _;
+  }
+//    function checkOwnership(address itemAddress) public{
+//        require(msg.sender == itemList[itemAddress].ownerAddress || msg.sender == address(this));
+//    }
+function removeItem(address _itemAddress) public onlyBy(_itemAddress) {
+    //checkOwnership(itemAddress);
+    uint rowToDelete = itemList[_itemAddress].index;
+    address keyToMove = itemIndex[itemIndex.length-1];
+    itemIndex[rowToDelete] = keyToMove;
+    itemList[keyToMove].index = rowToDelete;
+    itemIndex.length--;
+  emit LogDeleteItem(
+        _itemAddress,
+        rowToDelete);
+}
+
+function getItem(address itemAddress)public constant returns (string name, string url, string location, uint index, address ownerAddress) {
+    return (itemList[itemAddress].iname, itemList[itemAddress].url, itemList[itemAddress].location, itemList[itemAddress].index, itemList[itemAddress].ownerAddress);
+}
+
+function getItemAtIndex(uint index) public constant returns(address itemAddress) {
+    return itemIndex[index];}
+
+}
