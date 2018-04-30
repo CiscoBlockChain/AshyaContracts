229bdaadde73d98e12216b15f962f84d26985119
sajustin <sajustin@cisco.com>
2018-04-30 16:09:22 +0200

    add test in solidity

--- contracts/ashyaRegistry.sol
+++ contracts/ashyaRegistry.sol
@@ -1,5 +1,33 @@
 pragma solidity ^0.4.19;
 
+contract AshyaDevice {
+    //address AshyaRegistryAddress= 0x35ef07393b57464e93deb59175ff72e6499450cf;
+    string[] urls;
+    uint AddingPrice = 0.0010 ether;
+    address public owner = msg.sender;
+    AshyaRegistry deviceObj;
+
+    modifier CheckPrice(){
+        require(msg.value == AddingPrice);
+        _;
+    }
+
+    function AshyaDevice(address AshyaRegistryAddress, string name, string location, string url) public payable CheckPrice() {
+        deviceObj = AshyaRegistry(AshyaRegistryAddress);
+        deviceObj.addItem.value(AddingPrice)(name,location,url);
+    
+    }
+
+    function AddUrl(string url)public payable CheckPrice(){
+        urls.push(url);
+        
+    }
+    function getUrlsCount()public constant returns(uint count) {
+        return urls.length;
+        
+    }
+}
+
 
 contract AshyaRegistry{
 
--- truffle.js
+++ truffle.js
@@ -1,25 +1,4 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = process.env.INFURA_MNEMONIC;
-var server = process.env.INFURA_SERVER;
-var main_server = process.env.INFURA_MAIN_SERVER;
 module.exports = {
-  networks: {
-    kovan: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, server);
-      },
-      network_id: 42
-    },
-    mainnet: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, main_server);
-      },
-      network_id: 41
-    },
-    development: {
-      host: "127.0.0.1",
-      port: 9545,
-      network_id: 43
-    }
-  }
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
 };
