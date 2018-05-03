f73a2dfb7980c8b24478f3527112d24f56f67bd6
sajustin <sajustin@cisco.com>
2018-05-03 09:13:12 +0200

    restore truffle.js file

--- truffle.js
+++ truffle.js
@@ -1,4 +1,25 @@
-module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+var HDWalletProvider = require("truffle-hdwallet-provider");
+ var mnemonic = process.env.INFURA_MNEMONIC;
+ var server = process.env.INFURA_SERVER;
+ var main_server = process.env.INFURA_MAIN_SERVER;
+ module.exports = {
+   networks: {
+     kovan: {
+       provider: function() {
+         return new HDWalletProvider(mnemonic, server);
+       },
+       network_id: 42
+     },
+     mainnet: {
+       provider: function() {
+         return new HDWalletProvider(mnemonic, main_server);
+       },
+       network_id: 41
+     },
+     development: {
+       host: "127.0.0.1",
+       port: 9545,
+       network_id: 43
+     }
+   }
+ };
